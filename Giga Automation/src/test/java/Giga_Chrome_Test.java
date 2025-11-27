cb9b7a3adc607c7c80227356287d15895d613c86
Philip Ridgers <philridgers@gmail.com>
2025-11-27 11:51:23 +0000

    Automation for Signing Up and Adding Tickets, and Update to Seed Database with Valid Login for User

--- Giga Automation/src/test/java/Giga_Chrome_Test.java
+++ Giga Automation/src/test/java/Giga_Chrome_Test.java
@@ -1,12 +1,12 @@
 //import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.TakesScreenshot;
+import com.sun.source.tree.AssertTree;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.io.File;
-import org.openqa.selenium.OutputType;
-import org.apache.commons.io.FileUtils;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Giga_Chrome_Test {
     private static ChromeDriver driver;
@@ -14,7 +14,6 @@ public class Giga_Chrome_Test {
     @BeforeAll
     static void launchBrowser() {
         driver = new ChromeDriver();
-
     }
 
     @AfterAll
@@ -22,13 +21,42 @@ public class Giga_Chrome_Test {
         driver.quit();
     }
 
-    @Test
-     void attemptLoginWithoutPassword() throws Exception {
-        driver.get("http://127.0.0.1:5001/login");
-        driver.findElement(By.name("username")).click();
-        driver.findElement(By.name("username")).sendKeys("username");
-        driver.findElement(By.cssSelector(".col-lg-10 > input")).click();
-        Thread.sleep(2000);
+    @DisplayName("Adding items to Todo list: React")
+    @ParameterizedTest(name = "Adding item {0} and {1}, which tests {3}")
+    @CsvFileSource(resources = "/NewAccountItems.csv")
+     void attemptLoginTest(String username, String password, Boolean validity, String testPoint) throws Exception {
+        GigaPageObectModels giga = new GigaPageObectModels(driver);
+        giga.navigate("http://127.0.0.1:5001/login");
+        giga.SignUp();
+        giga.addUsername(username);
+        giga.addPassword(password);
+        giga.confirmPassword(password);
+//        Thread.sleep(1000);
+        if (validity) {
+            assertEquals("http://127.0.0.1:5001/login?signup_message=True", driver.getCurrentUrl());
+        } else {
+            assertEquals("http://127.0.0.1:5001/signup", driver.getCurrentUrl());
+        }
     }
 
+    @DisplayName("A COMPLETELY VALID TITLE FOR THIS")
+    @ParameterizedTest(name = "Play solder text")
+    @CsvFileSource(resources = "/TicketNumbers.csv")
+    void bookTicketsTest(String amount, Boolean validness, String testPoint) throws Exception {
+        GigaPageObectModels giga = new GigaPageObectModels(driver);
+        giga.navigate("http://127.0.0.1:5001/login");
+        giga.logIn();
+        Thread.sleep(1000);
+        giga.navigate("http://127.0.0.1:5001/gigs/1");
+        Thread.sleep(1000);
+        giga.bookTickets(amount);
+        Thread.sleep(1000);
+        if (validness) {
+            giga.navigate("http://127.0.0.1:5001/account");
+            giga.cancelBooking();
+            assertEquals("http://127.0.0.1:5001/account", driver.getCurrentUrl());
+        } else {
+            assertEquals("http://127.0.0.1:5001/book_gig/1", driver.getCurrentUrl());
+        }
+    }
 }
--- seeds/test_users.sql
+++ seeds/test_users.sql
@@ -9,5 +9,11 @@ CREATE TABLE users (
     hashed_password VARCHAR(255)
 );
 
--- password for "username" is "password"
-INSERT INTO users (username, hashed_password) VALUES ('username', 'scrypt:32768:8:1$GyJxMBmMStc53kgU$14c84080ba5cc61580ece80fb0df7859865f8b9e306d77d29f99f4b410fb845e27207bf2a466bb97c08d843b20be094cdfad1164e5b0a25136037627f63a87fd');
+
+-- password for "username" is "Passymcword!1"
+
+INSERT INTO users (username, hashed_password) VALUES ('username', 'scrypt:32768:8:1$XsRhdI77WT7dCncm$094eca39b6c5168b6874f53c24463926c9c267f2bac34e9d3a37246b4f52a439ebdf94897cf515b4426006760d681aa69e4ac417ac0033a6f1fb096501b98d75');
+-- password for "TooLong"
+INSERT INTO users (username, hashed_password) VALUES ('TooLong', 'scrypt:32768:8:1$2ZcoGqadWm30S0ry$70811ecc40037d783a5e6544534f980480fda812ec02149ab4e2c73ef16a8494ae09d5612d02a32bfe3693828bec5841213fc1c201020090a59175f85ad0aa6a');
+
+INSERT INTO users (username, hashed_password) VALUES ('admin', 'scrypt:32768:8:1$GyJxMBmMStc53kgU$14c84080ba5cc61580ece80fb0df7859865f8b9e306d77d29f99f4b410fb845e27207bf2a466bb97c08d843b20be094cdfad1164e5b0a25136037627f63a87fd');
